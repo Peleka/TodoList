4b033ed66ce5573bda9f8eccd426662118a601d4
Peleka <ego.iriska@gmail.com>
2021-10-29 10:27:05 +0300

    Updates

---
+++ precache-manifest.5358ddccf182b0dd7c9849c4b1009603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982e70c347894dcc260d2c24d808c5ff",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "ce1ea7f3e5a736e36559",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "0121cae34c8efbc1abb5",
+    "url": "/TodoList/static/js/2.58a8c900.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.58a8c900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161346213d26c6ce7f57",
+    "url": "/TodoList/static/js/3.8565141d.chunk.js"
+  },
+  {
+    "revision": "ce1ea7f3e5a736e36559",
+    "url": "/TodoList/static/js/main.00175041.chunk.js"
+  },
+  {
+    "revision": "126c3956fa10645d602a",
+    "url": "/TodoList/static/js/runtime-main.656b7d24.js"
+  }
+]);
